90e338302d376bc00cc5014d574454c38930250e
MrIngeenier <mrcodengineer@gmail.com>
2024-08-07 15:30:18 -0500

    Routas prueba 01

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import Principal from './pages/Principal';
 import HomePage from './pages/HomePage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//const homePath = process.env.REACT_APP_HOME_PATH || '/home';
+//const authPath = process.env.REACT_APP_AUTH_PATH || '/';
+
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+     {/* <Route path={homePath} element={<HomePage />} />
+      <Route path={authPath} element={<Principal />} />*/} 
+
         <Route path="/home" element={<HomePage />} />
-        <Route path="/auth" element={< Principal/>} />
+        <Route path="/" element={< Principal/>} />
       </Routes>
     </BrowserRouter>
   );
